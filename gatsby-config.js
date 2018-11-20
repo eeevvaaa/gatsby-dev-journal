53678fa9f9c931d5d661f9428797323eb03efeee
Eva <evalee904@gmail.com>
2018-11-20 00:59:54 -0600

    Blog name changed.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter Blog',
+    title: "Eva's Dev Journal",
     author: 'Kyle Mathews',
     description: 'A starter blog demonstrating what Gatsby can do.',
     siteUrl: 'https://gatsbyjs.github.io/gatsby-starter-blog/',
   },
-  pathPrefix: '/gatsby-starter-blog',
+  pathPrefix: '/evas-dev-journal',
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
